808a8388ff593ff4c17fc8f8cca6150f494328fa
Pragadesh Raj <114950806+Pragadesh000@users.noreply.github.com>
2023-05-16 12:04:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ then
   echo "Cloning main Repository"
   git clone https://github.com/LazyDeveloperr/LazyPrincess.git /LazyPrincess
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
   git clone $UPSTREAM_REPO /LazyPrincess
 fi
 cd /LazyPrincess
